fb3ec8158dc98770f6c327f87423dc95739b9d26
tktkaj <tktkaj@naver.com>
2024-08-01 19:09:37 +0900

    design: homeheader 디자인 완성
    - ideology 페이지 top버튼 끝으로 올라가게 수정
    - logo 누를시 홈으로 갈 수 있도록 a 태그 수정

--- src/main/webapp/resources/js/homepage.js
+++ src/main/webapp/resources/js/homepage.js
@@ -1,3 +1,16 @@
-$().ready(function (){
-    $("#login-btn").on("mouseover",fuction
-})
+$().ready(function(){
+	console.log("hello");
+	$(".opq-header").mouseover(function(){
+		$(".opq-header").css("background","white");
+		$(".opq-header a").css("color","black");
+		$("#login-btn").css("color","white");
+	});
+	
+	$("#uni-detail").mouseover(function(){
+		$(".opq-header-sub").slideDown(600);
+		$(".opq-header").css("background","white");
+		$(".opq-header a").css("color","black");
+		$("#login-btn").css("color","white");
+	});
+	
+});
