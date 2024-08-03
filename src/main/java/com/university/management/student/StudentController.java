cfbf33b6542faff8b3d2082ea2bbf1729850eca9
hong6425 <61792051+hong6425@users.noreply.github.com>
2024-08-04 01:58:56 +0900

    학적관리

--- src/main/java/com/university/management/student/StudentController.java
+++ src/main/java/com/university/management/student/StudentController.java
@@ -1,18 +1,56 @@
 package com.university.management.student;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.university.management.student.dto.Student;
+import com.university.management.student.service.StudentService;
 
 @Controller
 public class StudentController {
-
+	 @Autowired
+	  private HttpSession session;
+	
+	 @Autowired
+	 private StudentService stuservice;
+	 
 	@RequestMapping("/askpresident")
 	public String askpresident() {
 		return "student/askpresident";
 	}
 
 	@RequestMapping("/studentinfo")
-	public String management() {
+	public String studentinfo(Model  model) {
+		System.out.println("studentinfo실행");
+		
+		
+		  // 세션에서 loginname을 가져옴
+		Integer loginNo = (Integer) session.getAttribute("studentno");
+        System.out.println(loginNo);
+        // 세션에 loginname이 저장되어 있는지 확인
+        if (loginNo != null) {
+        	System.out.println("실행");
+            // 모델에 loginName을 추가
+        List<Student> studentInfo = stuservice.stuInfoSelect(loginNo);
+        
+        if(studentInfo.get(0).getSCH_DISCOUNT()==null) {
+       studentInfo.get(0).setSCH_DISCOUNT("해당없음");
+        }
+        	
+            System.out.println(studentInfo);
+            model.addAttribute("studentInfo", studentInfo);
+           
+        } else {
+          	System.out.println("노실행");
+            model.addAttribute("message", "로그인 정보가 없습니다.");
+        }
+
 		return "student/studentinfo";
 	}
 
